f5cce7ced45ed1e9773f0619c21c073a3161b0f1
HiepNCH <hoanghiep@Yaboku.local>
2023-05-05 23:14:51 +0700

    step 1

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/api/AsteroidApiService.kt
@@ -0,0 +1,41 @@
+package com.udacity.asteroidradar.api
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Constants.BASE_URL
+import com.udacity.asteroidradar.PictureOfDay
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+class AsteroidApiService {
+
+    interface AsteroidService {
+        @GET("neo/rest/v1/feed")
+        suspend fun getAsteroids(
+            @Query("api_key") api_key: String
+        ): String
+
+        @GET("planetary/apod")
+        suspend fun getPictureOfTheDay(
+            @Query("api_key") api_key: String
+        ): PictureOfDay
+    }
+
+    object AsteroidApi {
+        private val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        private val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        val retrofitService: AsteroidService by lazy {retrofit.create(AsteroidService::class.java)}
+    }
+}
